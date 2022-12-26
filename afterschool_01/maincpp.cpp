e3faba021bf28ea060a9451df9cf3115e77129e8
kye <kye6735@naver.com>
2022-12-26 11:36:55 +0900

    SFML 기본틀 완성

--- afterschool_01/maincpp.cpp
+++ afterschool_01/maincpp.cpp
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include  <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main(void) {
-	printf("hello world");
 
+	RenderWindow window(VideoMode(640, 480), "AfterSchool"); // 640*480 사이즈의 윈도우 창을 만듬
+	while (window.isOpen()) 
+	{
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed)
+				window.close();
+		}
+	}
 	return 0;
 }
