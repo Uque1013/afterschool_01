b38720bcb4f62dc03d16ee49617c964794ebbac4
kye <kye6735@naver.com>
2022-12-26 11:45:30 +0900

    문법 | switch 적용 전

---
+++ Moonbub/switch.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int a = 1;
+
+	if (a == 1)
+		printf("apple");
+	else if (a == 2)
+		printf("banana");
+	else
+		printf("mango");
+	return 0;
+}
--- afterschool_01/maincpp.cpp
+++ afterschool_01/maincpp.cpp
@@ -6,12 +6,15 @@ using namespace sf;
 int main(void) {
 
 	RenderWindow window(VideoMode(640, 480), "AfterSchool"); // 640*480 사이즈의 윈도우 창을 만듬
+	//윈도우가 열려있을 때까지 반복
 	while (window.isOpen()) 
 	{
 		Event event;
-		while (window.pollEvent(event)) {
+		while (window.pollEvent(event)) 
+		{
+			// 종료(x) 버튼을 누르면
 			if (event.type == Event::Closed)
-				window.close();
+				window.close(); // 윈도우를 닫는다 
 		}
 	}
 	return 0;
