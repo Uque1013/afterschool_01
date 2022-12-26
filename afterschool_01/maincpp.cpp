f15ec544b712cac09229e75a709c7ff1c3f36101
kye <kye6735@naver.com>
2022-12-26 14:04:50 +0900

    플레이어 색깔 변경

--- afterschool_01/maincpp.cpp
+++ afterschool_01/maincpp.cpp
@@ -10,7 +10,7 @@ int main(void) {
 	RectangleShape player;
 	player.setSize(Vector2f(40, 40));
 	player.setPosition(100, 100);
-	
+	player.setFillColor(Color::Red);
 
 	//윈도우가 열려있을 때까지 반복
 	while (window.isOpen())
