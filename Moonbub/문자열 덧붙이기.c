07cc11453d97297c39b56114d96fa12026e317ec
kye <kye6735@naver.com>
2022-12-27 11:18:54 +0900

    문법 | 문자열 덧붙이기 끝

--- Moonbub/문자열 덧붙이기.c
+++ Moonbub/문자열 덧붙이기.c
@@ -4,14 +4,10 @@
 
 int main(void)
 {
-	char str[30] = "score : ";
+	char str[30];
 	int score = 300;
+	sprintf(str, "score : %d", score);
 
-	char str2[30];
-	// 정수를 문자열로 (변환할 정수, 적용대상, 진수)
-	itoa(score, str2,10);
-
-	strcat(str, str2);
 	printf("%s\n", str);
 
 	return 0;
