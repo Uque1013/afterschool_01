29477413fbd4487fe0ef4c5cfd7111ec20a50c48
kye <kye6735@naver.com>
2022-12-27 10:48:32 +0900

    문법 | 문자열 덧붙이기 3

--- Moonbub/문자열 덧붙이기.c
+++ Moonbub/문자열 덧붙이기.c
@@ -4,17 +4,9 @@
 int main(void)
 {
 	char str[30] = "score : ";
-	//str의 글자 수 구하기
-	int str_len = strlen(str);
-
-
 	char str2[30] = "300";
-	// str2의 글자 수 구하기
-	int str2_len = strlen(str2);
 
-	for (int i = 0; i < str2_len; i++)
-		str[i + str_len] = str2[i];
-	str[str_len + str2_len] = '\0';
+	strcat(str, str2);
 	printf("%s\n", str);
 
 	int score = 300;
