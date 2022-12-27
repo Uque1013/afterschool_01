0a6fd1dd712046d33eafd2779f8dea8fc67c8520
kye <kye6735@naver.com>
2022-12-27 14:32:32 +0900

    문법 | 구조체 적용 전

---
+++ Moonbub/구조체.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int player_hp = 5;
+	int player_speed = 7;
+
+	int enemy_hp = 2;
+	int enemy_speed = 5;
+
+	printf("플레이어 체력:%d 플레이어 속도:%d\n", player_hp, player_speed);
+
+	return 0;
+}
--- Moonbub/문자열 덧붙이기.c
+++ Moonbub/문자열 덧붙이기.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <string.h> // 문자열 라이브러리
-#include <stdlib.h> // itoa 사용
-
-int main(void)
-{
-	char str[30];
-	int score = 300;
-	sprintf(str, "score : %d", score);
-
-	printf("%s\n", str);
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <string.h> // 문자열 라이브러리
+//#include <stdlib.h> // itoa 사용
+//
+//int main(void)
+//{
+//	char str[30];
+//	int score = 300;
+//	sprintf(str, "score : %d", score);
+//
+//	printf("%s\n", str);
+//
+//	return 0;
+//}
