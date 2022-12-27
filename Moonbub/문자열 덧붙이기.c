a06dabba470a5a3f89821ce66a4013a23395f0c0
kye <kye6735@naver.com>
2022-12-27 11:15:21 +0900

    문법 | 문자열 덧붙이기 4

--- Moonbub/문자열 덧붙이기.c
+++ Moonbub/문자열 덧붙이기.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> // 문자열 라이브러리
+#include <stdlib.h> // itoa 사용
 
 int main(void)
 {
 	char str[30] = "score : ";
-	char str2[30] = "300";
+	int score = 300;
+
+	char str2[30];
+	// 정수를 문자열로 (변환할 정수, 적용대상, 진수)
+	itoa(score, str2,10);
 
 	strcat(str, str2);
 	printf("%s\n", str);
-
-	int score = 300;
 
 	return 0;
 }
