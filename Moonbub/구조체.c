636d57efb06ba570f85af4948ed8bd061ae29f03
kye <kye6735@naver.com>
2022-12-28 14:20:04 +0900

    문법 | 함수

--- Moonbub/구조체.c
+++ Moonbub/구조체.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-
-struct Saram {
-	int hp;
-	int speed;
-};
-
-int main(void) {
-	struct Saram player;
-	player.hp = 5;
-	player.speed = 7;
-
-	struct Saram enemy;
-	enemy.hp = 2;
-	enemy.speed = 5;
-
-	printf("플레이어 체력:%d 플레이어 속도:%d\n", player.hp, player.speed);
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//struct Saram {
+//	int hp;
+//	int speed;
+//};
+//
+//int main(void) {
+//	struct Saram player;
+//	player.hp = 5;
+//	player.speed = 7;
+//
+//	struct Saram enemy;
+//	enemy.hp = 2;
+//	enemy.speed = 5;
+//
+//	printf("플레이어 체력:%d 플레이어 속도:%d\n", player.hp, player.speed);
+//
+//	return 0;
+//}
