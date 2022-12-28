f9bc9a2946698be97d1bbf126f08fa66184640e0
kye <kye6735@naver.com>
2022-12-28 14:32:18 +0900

    총알 관통버그 해결

---
+++ Moonbub/함수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// 함수의 구성요소 : 반환형 함수이름(매개변수)
+
+int f(int x)
+{
+	return 2 * x + 1;
+}
+
+int main(void) {
+	printf("%d", f(1));
+
+	return 0;
+}
--- afterschool_01/maincpp.cpp
+++ afterschool_01/maincpp.cpp
@@ -209,12 +209,9 @@ int main(void) {
 
 			if (enemy[i].life > 0)
 			{
-				// TODO : 총알이 관통하는 버그 수정할 것
-				//enemy와의 충돌
-				if (is_collide(player.sprite, enemy[i].sprite )|| 
-					is_collide(bullet.sprite,enemy[i].sprite))
+				// player, enemy 충돌
+				if (is_collide(player.sprite, enemy[i].sprite))
 				{
-					printf("enemy[%d]와 충돌\n", i);
 					enemy[i].life -= 1;
 					player.score += enemy[i].score;
 
@@ -230,9 +227,23 @@ int main(void) {
 					player.life -= 1;
 					enemy[i].life = 0;
 				}
+
+				// 총알과 enemy의 충돌
+				if (is_collide(bullet.sprite, enemy[i].sprite))
+				{
+					enemy[i].life -= 1;
+					player.score += enemy[i].score;
+
+					// TODO : 코드 refactoring 필요
+					if (enemy[i].life == 0)
+					{
+						enemy[i].explosion_sound.play();
+					}
+					bullet.is_fired = 0;
+				}
+
 				enemy[i].sprite.move(enemy[i].speed, 0);
 			}
-			
 		}
 
 		// TODO : 총알이 평생 한 번만 발사되는 버그 수정하기
