9f871f511fe29869bd7be2eb4987b1a10481dfb2
kye <kye6735@naver.com>
2022-12-30 13:48:43 +0900

    입력정보 GUI로

--- AfterSchool2/main.cpp
+++ AfterSchool2/main.cpp
@@ -14,6 +14,14 @@ int main(void)
 	Vector2i mouse_pos;
 	int click_cnt=0; // 마우스 누른 횟수
 
+	Font font;
+	font.loadFromFile("c:/Windows/Fonts/arial.ttf");
+	Text text;
+	text.setFont(font);
+	text.setCharacterSize(30);
+	text.setFillColor(Color::White);
+	text.setPosition(0, 0);
+	char info[40];
 
 	while (window.isOpen())
 	{
@@ -29,19 +37,22 @@ int main(void)
 				break;
 				// 한 번 누르면 한 번만 적용
 			case Event::MouseButtonPressed:
-				if (event.mouseButton.button == Mouse::Right)
+				if (event.mouseButton.button == Mouse::Left)
 				{
 					click_cnt++;
 				}
 			}
 		}
-		// 마우스 누른 상태면 여러번 증가함	
-		if (Mouse::isButtonPressed(Mouse::Left))
-		{
-			click_cnt++;
-		}
-		printf("(%d, %d) 클릭횟수 %d \n"
-			, mouse_pos.x, mouse_pos.y,click_cnt);
+
+		sprintf(info, "(%d, %d) clicks %d \n"
+			, mouse_pos.x, mouse_pos.y, click_cnt);
+		text.setString(info);
+
+		window.clear(Color::Black);
+
+		window.draw(text);
+
+		window.display();
 	}
 
 	return 0;
