ef59f18dc35dee60a84ce2a6859187697d8ca479
kye <kye6735@naver.com>
2023-01-02 11:48:09 +0900

    문법 | swap이 잘되는 이유

--- Moonbub/swap.c
+++ Moonbub/swap.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-swap(int x, int y)
+// -a, _b는 주소값, *_a, *_b는 가리키는 변수의 실제값(int)
+void swap(int* _a, int* _b)
 {
-	int swap = x;
-	int b = y;
-	int a = x;
+	int temp;
+	temp = *_a;
+	*_a = *_b;
+	*_b = temp;
 }
 
 int main(void)
 {
-	int a = 3;
-	int b = 5;
+	int a = 3, b = 5;
 
 	printf("a:%d b:%d \n", a, b);
 
-	// a와 b를 swap하는 함수를 호출
+	swap(&a, &b); // _a에게 a의 주소값을, _b에게 b의 주소값을 넘겨줌
 
-	printf("a:%d b:%d \n", swap(a, b));
+	printf("a:%d b:%d \n", a, b);
 
 	return 0;
 }
