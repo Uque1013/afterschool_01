7b371b00e6a211fc05b582de976fbebd5a20bc7a
kye <kye6735@naver.com>
2023-01-04 10:10:13 +0900

    점프가 끝나면 착지

--- Moonbub/Animation.cpp
+++ Moonbub/Animation.cpp
@@ -5,11 +5,16 @@ using namespace sf;
 
 struct Player {
 	RectangleShape sprite;
-	int idx;					// 애니메이션 index
+	int idx;							// 애니메이션 index
 	int frames;					// 애니메이션 frame수
 	long ani_time;				// 애니메이션이 바뀔 때의 시각
 	long ani_delay;			// 화면 전환의 텀
+
 	int speed;
+	// 점프 관련 변수
+	int is_jumping;			// 점프상태인가?
+	int jumping_time;		// 점프를 시작하는 시각
+	int jump_speed;
 };
 
 int main(void)
@@ -18,9 +23,9 @@ int main(void)
 	window.setFramerateLimit(60);
 
 	long start_time;
-	long spent_time;
+	long spent_time;				// 게임진행 시각
 
-	const int GRAVITY = 10; // 중력
+	const int GRAVITY = 5; // 중력
 	const int PLATFORM_Y = 500; // 땅바닥의 y좌표
 
 	Texture run[10];
@@ -43,9 +48,11 @@ int main(void)
 	player.sprite.setPosition(200, 200);
 	player.ani_delay = 100 / player.frames / 2; // 0.5초마다 걸음
 	player.speed = 5;
+	player.jump_speed = GRAVITY * 2; // 일정한 속도로 올라가고 내려감
 
 	start_time = clock();
 	player.ani_time = start_time;
+	player.jumping_time = start_time;
 
 	while (window.isOpen())
 	{
@@ -62,10 +69,9 @@ int main(void)
 				if (event.key.code == Keyboard::Space)
 				{
 					// 점프
-					player.sprite.move(0, -3);
+					player.is_jumping = 1;
+
 				}
-				break;
-			default:
 				break;
 			}
 		}
@@ -89,7 +95,18 @@ int main(void)
 			player.idx++;
 		}
 
+		// 필요하면 1000을 변수 처리할 것
+		if (spent_time - player.jumping_time > 1000)
+		{
+			player.is_jumping = 0;
+		}
+
 		player.sprite.move(0, GRAVITY); // 중력 작용
+
+		if (player.is_jumping == 1)
+		{
+			player.sprite.move(0, -player.jump_speed);
+		}
 
 		// 플레이어가 땅바닥에 착지하면
 		if (player.sprite.getPosition().y + player.sprite.getSize().y > PLATFORM_Y)
